94f696cd86dfd529cf533b5bd1333ee0610bc7c2
Emre Tataroglu <tatarogluemre25@gmail.com>
2022-11-29 23:55:07 +0300

    v4

--- src/Lambda04.java
+++ src/Lambda04.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Lambda04 {
@@ -18,11 +15,12 @@ public class Lambda04 {
     public static void main(String[] args) {
 
         Universite u01 = new Universite("Boğaziçi", "Matematik", 577, 93.00);
+        Universite u05 = new Universite("Atatürk", "Matematik", 233, 33.00);
         Universite u02 = new Universite("Çanakkale", "Bilgisayar", 54, 99.00);
-        Universite u03 = new Universite("Ktu", "Hukuk", 77, 70.00);
-        Universite u04 = new Universite("İstanbul", "Tıp", 345, 66.00);
+        Universite u03 = new Universite("Ktu", "Hukuk", 770, 70.00);
+        Universite u04 = new Universite("İstanbul", "Tıp", 650, 66.00);
 
-        List<Universite> unv = new ArrayList<>(Arrays.asList(u01, u02, u03, u04));
+        List<Universite> unv = new ArrayList<>(Arrays.asList(u01, u02, u03, u04,u05));
 
 
         System.out.println(notOrt74BykUnv(unv));
@@ -32,6 +30,15 @@ public class Lambda04 {
         } else System.out.println("Mat bölüm yok");
 
         System.out.println(ogrSayisiBykSirala(unv));
+
+        System.out.println(matBolSay(unv));
+
+        unvOgrSAy550FzlNotOrt(unv);
+
+        System.out.println(unvOgrSAy550FzlNotOrtalamasi(unv));
+
+        System.out.println(ogrsay1050azEnkckNotOrt(unv));
+
     }
 
     //task 01--> Bütün Üniv.lerin notOrt'larinin 74' den buyuk old kontrol eden pr create ediniz.
@@ -59,6 +66,46 @@ public class Lambda04 {
                 // universiteler öğrenci sayısına göre tersden sıralandı
                         collect(Collectors.toList()); // Stream yapısı List yapısına dönüştürüldü
     }
+
+// task 04 --> universitelerin matematik bölüm saysısı
+    public static int matBolSay(List<Universite> unv){
+
+        return (int) unv.stream().filter(t -> t.getBolum().toLowerCase().contains("mat")).count();
+
+
+    }
+//task 05-->Ogrenci sayilari 550'dan fazla olan universite'lerin en buyuk notOrt'unu bulunuz.
+
+    public static void unvOgrSAy550FzlNotOrt(List<Universite> unv){
+
+       unv.
+               stream().
+               filter(t -> t.getOgrSayisi() > 550).
+               sorted(Comparator.comparing(Universite::getNotOrtalamasi).reversed()).
+               limit(1).
+               forEach(t-> System.out.println(t));
+
+
+    }
+
+    public static OptionalDouble unvOgrSAy550FzlNotOrtalamasi(List<Universite> unv){
+
+       return unv.
+               stream().
+               filter(t-> t.getOgrSayisi()>550).
+               mapToDouble(t->t.getNotOrtalamasi()).//max methodunu kullanmak için maptoInt() veya mapToDouble data type göre kullandık
+               max();// nullPointerException atmaması için max() return type OptionalInt veya OptıonalDouble dır data type göre
+
+
+    }
+
+    public static OptionalDouble ogrsay1050azEnkckNotOrt(List<Universite> unv){
+
+
+       return unv.stream().filter(t-> t.getOgrSayisi()<1050).mapToDouble(t-> t.getNotOrtalamasi()).min();
+
+    }
+
 }
 
 
---
+++ src/Lambda05.java
@@ -0,0 +1,83 @@
+import java.util.stream.IntStream;
+
+public class Lambda05 {
+    public static void main(String[] args) {
+
+        System.out.println(topla(10));
+        System.out.println(toplaFunc(10));
+        System.out.println(toplaCift(10));
+        System.out.println(toplaCift(6));
+        System.out.println(ilkXTekTopla(6));
+        print2Kuvveti(4);
+
+    }
+
+    //TASK 01 --> Structured Programming ve Functional Programming ile 1'den x'e kadar (x dahil) tamsayilari toplayan
+    //            bir program create ediniz.
+
+    //Structured
+
+    public static int topla(int x){
+
+        int topla = 0;
+
+        for(int i = 0 ; i<=x ; i++){
+
+            topla += i;
+
+        }
+
+        return topla;
+    }
+
+//functional
+
+    public static int toplaFunc(int x){
+
+        return IntStream. //Lambda da List olmayan primitiv int data ların akışını sağlamak için IntStream kullanılır
+                range(1,x+1). //range(a,b) metodu  a dan b ye kadar(b dahil değil) olan değer leri akışa alır.
+                sum(); //akıştaki elemanları toplar.
+
+
+
+    }
+
+    //TASK 02 --> 1'den x'e kadar cift tamsayilari toplayan bir program create ediniz.
+
+    public static int toplaCift(int x){
+
+        return
+                IntStream.
+                rangeClosed(1,x).//rangeClosed methodu(a,b) adan b dahil olmak üzere akışa alır.
+                filter(Lambda01::cift).
+                sum();
+
+    }
+
+    //TASK 03 --> Ilk x pozitif cift sayiyi toplayan program  create ediniz.
+
+    public static int ilkXCiftTopla(int x){
+
+        return IntStream.
+                iterate(2,t-> t+2).//2 den sonsuza kadar elemanları 2 artırarak akışa alır.> 2,4,6,8,....
+                limit(x).//sonsuza kadar gitmemesi için x e kadar sınırlıyoruz
+                sum();
+
+    }
+
+  //TASK 04 --> Ilk X pozitif tek tamsayiyi toplayan programi  create ediniz.
+
+    public static int ilkXTekTopla(int x){
+
+        return IntStream.iterate(1,t-> t+2).limit(x).sum();
+    }
+
+    //TASK 05 --> 2'nin ilk pozitif x kuvvetini ekrana yazdiran programi  create ediniz.
+
+    public static void print2Kuvveti(int x){
+
+        IntStream.iterate(2,t->t*2).limit(x).forEach(t-> System.out.print(t+" "));
+
+    }
+
+}
